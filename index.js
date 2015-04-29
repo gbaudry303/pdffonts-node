666508a97a5323f3d89f288d5ddfab01179700f0
gbaudry303 <gbaudry303@gmail.com>
2015-04-29 16:44:06 -0700

    fixed method, working as intented, wrote test

--- index.js
+++ index.js
@@ -5,49 +5,35 @@ var exec = require('child_process').exec;
 module.exports = {
   process: function(pdfFileName, next) {
     // The output of the system call to pdffonts needs to be treated to be able to parse.
-    // var endResult = ['fsdfs'];
+    var endResult = [];
 
-    // exec('pdffonts /home/guillaume/pdffonts-node/test/test.pdf', function (error, stdout, stderr) {
-    exec('pdffonts /home/guillaume/pdffonts-node/test/test.pdf', function (error, stdout, stderr) {
-      console.log(error);
-      console.log(stdout);
-      console.log(stderr);
+    exec('pdffonts ' + pdfFileName, function (error, stdout, stderr) {
+      if (error) return next(error);
 
-      if (err) return next(err);
-      if (stderr) return next(stderr); // if stderr is an error obj
-     return next(null, stdout);
+      var outputString = String(stdout);
+      var outputString = outputString.replace(/\sType\s/g, '-Type-');  //replace space in type with - because of split on space later
+      var headerStrippedOutput = outputString.split('--').pop();
+      var arrayFontsDataRow = headerStrippedOutput.split('\n');
+
+      arrayFontsDataRow.splice(0,1);
+      arrayFontsDataRow.splice(-1,1);
+
+      for (var i=0,  tot=arrayFontsDataRow.length; i < tot; i++) {
+          var tmpArray = arrayFontsDataRow[i].split(/\s+/);
+          endResult.push({
+            'name': tmpArray[0],
+            'type': tmpArray[1],
+            'encoding': tmpArray[2],
+            'embedded': tmpArray[3] === 'yes' ? true : false,
+            'subset': tmpArray[4] === 'yes' ? true : false,
+            'unicode': tmpArray[5] === 'yes' ? true : false,
+            'object': tmpArray[6],
+            'id': tmpArray[7]
+        });
+      }
+
+     return next(null, endResult);
     });
 
-    // console.log("dasdasdas")
-    // exec('pdffonts ' + pdfFileName, function (error, stdout, stderr) {
-    //   console.log(error);
-    //   var outputString = String(stdout);
-    //   var outputString = outputString.replace(/\sType\s/g, '-Type-')
-    //   // console.log(outputString);
-
-    //   var headerStrippedOutput = outputString.split('--').pop();
-    //   var arrayFontsDataRow = headerStrippedOutput.split('\n');
-    //   arrayFontsDataRow.splice(0,1);
-    //   arrayFontsDataRow.splice(-1,1);
-
-    //   for (var i=0,  tot=arrayFontsDataRow.length; i < tot; i++) {
-    //       var tmpArray = arrayFontsDataRow[i].split(/\s+/);
-    //       endResult.push({
-    //         'name': tmpArray[0],
-    //         'type': tmpArray[1],
-    //         'encoding': tmpArray[2],
-    //         'embedded': tmpArray[3] === 'yes' ? true : false,
-    //         'subset': tmpArray[4] === 'yes' ? true : false,
-    //         'unicode': tmpArray[5] === 'yes' ? true : false,
-    //         'object': tmpArray[6],
-    //         'id': tmpArray[7]
-    //     });
-    //   }
-    //   // console.log(endResult);
-    //   return endResult;
-    // });
-    // return;
-    // return endResult;
   }
-
 };
--- test/index.js
+++ test/index.js
@@ -3,11 +3,20 @@ var should = require('chai').should(),
     getFonts = pdffonts.process;
 
 describe('process methods of module', function() {
-  it('get fonts with valid properties', function() {
-    // var result = getFonts('/home/guillaume/pdffonts-node/test/test.pdf');
-    getFonts('/test/test.pdf', function(err, result) {
+  it('get fonts with valid properties', function(done) {
+    getFonts('test/test.pdf', function(err, result) {
       console.log(result);
       result.should.be.a('array');
+      result[0].should.have.property('name');
+      result[0].should.have.property('type');
+      result[0].should.have.property('encoding');
+      result[0].should.have.property('embedded');
+      result[0].should.have.property('subset');
+      result[0].should.have.property('unicode');
+      result[0].should.have.property('object');
+      result[0].should.have.property('id');
+
+      return done();
    });
   });
 });
